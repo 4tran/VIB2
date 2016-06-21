cd19631b5fe9060d8c0ae47d64e019775c816ed6
QuakersOat <quakersoat@gmail.com>
2016-06-21 11:00:54 -0400

    Database connection

--- res/config.php
+++ res/config.php
@@ -1,3 +1,9 @@
 <?php
 $config = json_decode(file_get_contents("../config.json"), true);
+if ($config['pass'] == 'password') {
+  echo 'Please configure your config.json. If, when you refresh this page, you get a 500 error, that means you have misconfigured your config.php.';
+}
+else {
+  $db = new PDO('mysql:host=' . $config['ip'] . ';dbname=' . $config['db'] . ';charset=utf8mb4', $config['user'], $config['pass']);
+}
 ?>
