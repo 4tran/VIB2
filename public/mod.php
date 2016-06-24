549892a7816551584e5d268e3f6ec8c64aff7182
QuakersOat <quakersoat@gmail.com>
2016-06-24 11:14:23 -0400

    Login still not working.

--- public/mod.php
+++ public/mod.php
@@ -3,12 +3,14 @@ require_once '../res/config.php';
 require_once $config['root'] . '/res/twig_loader.php';
 
 // Log in
-if ($_COOKIE['admin']){
+if ($_COOKIE['admin'] == "false" || $_COOKIE['admin'] == NULL) {
     echo $twig->render('mod/mod_login.html', array('title' => $config['site_name']));
 }
 
-if (isset($_GET['create_board'])) {
-  echo $twig->render('mod/create_board.html', array('title' => $config['site_name']));
+if ($_COOKIE['admin'] == "true") {
+    if (isset($_GET['create_board'])) {
+        echo $twig->render('mod/create_board.html', array('title' => $config['site_name']));
+    }
 }
 ?>
 <html>
