d6f33dfaef78dc1f5806c30003b591054e77fe3c
QuakersOat <quakersoat@gmail.com>
2016-06-25 18:31:10 -0400

    Finished very basic authentication (admin account only)

--- public/mod.php
+++ public/mod.php
@@ -1,23 +1,26 @@
 <?php
 require_once '../res/config.php';
 require_once $config['root'] . '/res/twig_loader.php';
-
-// Log in
-if ($_COOKIE['admin'] == "false" || $_COOKIE['admin'] == NULL) {
-    echo $twig->render('mod/mod_login.html', array('title' => $config['site_name']));
-}
-
-if ($_COOKIE['admin'] == "true") {
-    if (isset($_GET['create_board'])) {
-        echo $twig->render('mod/create_board.html', array('title' => $config['site_name']));
-    }
-}
 ?>
 <html>
 <head>
   <title>Mod panel</title>
 </head>
 <body>
-<a href='mod.php?create_board'>Create a new board</a>
-</body>
-</html>
+<?php
+// Log in
+if ($_COOKIE['admin'] == "false" || $_COOKIE['admin'] == NULL) {
+    echo $twig->render('mod/mod_login.html', array('title' => $config['site_name']));
+}
+
+if ($_COOKIE['admin'] == "true") {
+    echo "<a href=\"mod.php?create_board\">Create a new board</a>";
+    
+    if (isset($_GET['create_board'])) {
+        echo $twig->render('mod/create_board.html', array('title' => $config['site_name']));
+    }
+}
+?>
+ </body>
+ </html>
+
--- public/mod/mod_login.php
+++ public/mod/mod_login.php
@@ -9,11 +9,11 @@ $query->execute();
 $users = $query->fetchAll();
 
 if ($username == $users[0]['username'] && password_verify($password, $users[0]['password'])) {
-    setcookie("admin", "true", time()+3600);
+    setcookie("admin", "true", time()+3600, "/");
     header("Location: /mod.php");
 }
 else {
-    setcookie("admin", "false", time()+3600);
+    setcookie("admin", "false", time()+3600, "/");
     echo "Incorrect username or password";
 }
 ?>
