c1d0fedec42c5e7a249c166d870c94504fa61607
QuakersOat <quakersoat@gmail.com>
2016-06-26 11:11:12 -0400

    Finished board index (for now)

--- templates/board_index.php
+++ templates/board_index.php
@@ -8,13 +8,28 @@ $uri = $board_config['uri'];
 $title = $board_config['title'];
 $subtitle = $board_config['subtitle'];
 
-// Get post data from database.
-$query = "select * from posts where uri = :uri";
+// Get a list of original posts.
+$query = "select id from posts where uri = :uri and id = op order by bump desc";
 $query = $db->prepare($query);
 $query->bindParam(':uri', $uri);
 $query->execute();
-$posts = $query->fetchAll();
+$op_ids = $query->fetchAll();
 
+// Get last 3 replies from threads.
+$query = "select * from posts where uri = :uri and id = :op  union select * from (select * from posts where uri = :uri and op = :op order by id desc limit 3) x     order by id asc";
+$query = $db->prepare($query);
+$query->bindParam(':uri', $uri);
+for ($i = 0; $i < count($op_ids); $i++) {
+    $query->bindParam(':op', $op_ids[$i]['id']);
+    $query->execute();
+    if ($i < 1) {
+        $posts = preg_replace("/(}])/mi", "},", json_encode($query->fetchAll()));
+    }
+    else {
+        $posts .= preg_replace("/(\\[{)/mi", "{", json_encode($query->fetchAll()));
+    }   
+}
+$posts = json_decode($posts);
 // After all the logic is done, render the index.
 echo $twig->render('board_index.html', array(
     'title' => $title,
