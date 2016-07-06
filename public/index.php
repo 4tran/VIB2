9c38fd5c3331d95130a1cd55b98bb40ec216ca16
QuakersOat <quakersoat@gmail.com>
2016-07-05 20:23:15 -0400

    Site index fix and board index pages. Next will be post auto-deletion.

--- public/index.php
+++ public/index.php
@@ -8,15 +8,15 @@ $query->execute();
 $boards = $query->fetchAll();
 
 // Get list of latest posts.
-$query = $db->prepare("select uri, content, id, op from posts order by id desc limit 10");
+$query = $db->prepare("select uri, content, id, op from posts order by timestamp desc limit 15");
 $query->execute();
 $posts = $query->fetchAll();
 for ($i = 0; $i < count($posts); $i++) {
-    $posts[$i]['content'] = substr(trim(preg_replace("/\s+/", " ", $posts[$i]['content'])), 0, 150);
+    $posts[$i]['content'] = substr(strip_tags(trim(preg_replace("/\s+/", " ", $posts[$i]['content']))), 0, 150);
 }
 
 // Get list of latest images.
-$query = $db->prepare("select * from (select thumbnail, op, id, uri from posts where thumbnail <> '' order by id desc) x limit 5");
+$query = $db->prepare("select * from (select thumbnail, op, id, uri from posts where thumbnail <> '' order by timestamp desc) x limit 5");
 $query->execute();
 $images = $query->fetchAll();
 
--- templates/board_index.php
+++ templates/board_index.php
@@ -12,20 +12,32 @@ $board_config = json_decode(file_get_contents("index.json"), true);
 $uri = $board_config['uri'];
 $title = $board_config['title'];
 $subtitle = $board_config['subtitle'];
+$page = $_POST['page'];
 
-// Get a list of original posts.
-$query = "select id from posts where uri = :uri and id = op order by bump desc";
-$query = $db->prepare($query);
-$query->bindValue(':uri', $uri);
-$query->execute();
-$op_ids = $query->fetchAll();
+// Function to get list of ops for selected page.
+function loadPage($page) {
+	global $op_ids, $db, $uri;
+    $limit = 10*$page;
+    $query = $db->prepare("select id from (select * from (select * from posts where uri = :uri and id = op order by bump desc limit :limit) x order by bump asc limit 10) x order by bump desc");
+    $query->bindValue(':uri', $uri);
+    $query->bindValue(':limit', $limit, PDO::PARAM_INT);
+    $query->execute();
+    $op_ids = $query->fetchAll();
+}
+// If a page isn't selected, assume page 1.
+if (empty($page)) {
+	$page = 1;
+}
+
+// Load the page's ops.
+loadPage($page);
 
 // Ensure there are posts, otherwise errors will be thrown.
 if (count($op_ids) == 0) {
     $posts[0]['content'] = 'No posts yet';
 }
 // If there are posts, go ahead and display them.
-else {
+if (count($op_ids) > 0) {
     // Get last 3 replies from thread.
     for ($i = 0; $i < count($op_ids); $i++) {
         $id = $op_ids[$i][0];
